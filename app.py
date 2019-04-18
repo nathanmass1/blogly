1035bfeb5836b3f2522998562b2509e3054d93df
Austin <austin.yau@gmail.com>
2019-04-17 17:09:37 -0700

    further study material

--- app.py
+++ app.py
@@ -21,51 +21,60 @@ def home_page():
 
     return redirect("/users")
 
+
 @app.route("/users")
 def user_page():
 
-    users = User.query.all()
+    users = User.query.order_by('last_name')
+    print("TESTTTT")
+    print("User Query is", User.query)
+
     return render_template("users.html", users=users)
+
 
 @app.route("/users/new")
 def new_user_page():
 
     return render_template("add_user.html")
 
-@app.route("/users",  methods=["POST"])
-def create_user():    
 
+@app.route("/users",  methods=["POST"])
+def create_user():
 
     first_name = request.form.get("first_name", "")
     last_name = request.form.get("last_name", "")
     image_url = request.form.get("image_url", "")
     # print(first_name, last_name, image_url)
     # print("LOOK AT ME")
-    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    user = User(first_name=first_name,
+                last_name=last_name, image_url=image_url)
     db.session.add(user)
     db.session.commit()
 
     return redirect("/users")
 
+
 @app.route("/users/<id>")
 def get_user(id):
 
     user = User.query.get(id)
-    print(id)
-    print("LOOK AT ME")
-    first_name = user.first_name
-    last_name = user.last_name
+   #  print(id)
+   #  print("LOOK AT ME")
+   #  first_name = user.first_name
+   #  last_name = user.last_name
     image_url = user.image_url
 
-    return render_template("profile.html", first_name=first_name, last_name=last_name, image_url=image_url, id=id)
+    return render_template("profile.html", user=user, image_url=image_url, id=id)
+
 
 @app.route("/users/<id>/edit")
-def edit_user(id):      
-   print("TEST", id) 
-   return render_template("edit_user.html", id=id)
+def edit_user(id):
+    print("TEST", id)
+    return render_template("edit_user.html", id=id)
+
 
 @app.route("/users/<id>/edit", methods=["POST"])
-def edit_user_submit(id):      
+def edit_user_submit(id):
 
     first_name = request.form.get("first_name", "")
     last_name = request.form.get("last_name", "")
@@ -81,10 +90,10 @@ def edit_user_submit(id):
     db.session.commit()
 
     return redirect("/users")
-    
+
 
 @app.route("/users/<id>/delete", methods=["POST"])
-def delete_user(id):      
+def delete_user(id):
 
     # first_name = request.form.get("first_name", "")
     # last_name = request.form.get("last_name", "")
--- models.py
+++ models.py
@@ -16,10 +16,14 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer,
-                    primary_key=True,
-                    autoincrement=True)
+        primary_key=True,
+        autoincrement=True)
     first_name = db.Column(db.String(50),
-                        nullable=False)
+        nullable=False)
     last_name = db.Column(db.String(50),
-                        nullable=False)                 
+        nullable=False)                 
     image_url = db.Column(db.String(100), nullable=False, default="https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP1477-CUSA07022_00-AV00000000000007//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100")
+
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
